e37e51f893d45f11b2cd0e4d4e2c952cc55789f2
Kacper <kapper2137@gmail.com>
2022-06-23 00:45:50 +0200

    project is almost ready

--- js/main.js
+++ js/main.js
@@ -3,10 +3,8 @@ const footerYear = document.querySelector('.footer__year')
 
 
 const handleCurrentYear = () => {
-	// taki zapis aby footer sie aktualizowawl, jezeli chcemy ja wywolac to trzeba dodac otwarte nawiasy (na dole)
 	const year = new Date().getFullYear()
 	footerYear.innerText = year
-	// inner text = year, bo year to nazwa zmiennej
 }
 
 handleCurrentYear()
